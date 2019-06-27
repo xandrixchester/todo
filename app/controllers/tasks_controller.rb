404547398b8ef8d747e1943cbb28c68be8ecf1bc
xandrix <xandrixecklor@hotmaill.com>
2019-06-27 03:04:27 +0000

    Update Tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,5 +4,18 @@ class TasksController < ApplicationController
 		render json: Task.all 
 	end
 
+
+	def update
+		task = Task.find(params[:id])
+		task.update_attributes(task_params)
+		render json: task
+	end
+
+	private
+
+	def task_params
+		params.require(:task).permit(:done)
+	end
+
 	
 end
