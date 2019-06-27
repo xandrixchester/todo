fa540ea99fec2854872d0bfc79f268cc8ca19a25
xandrix <xandrixecklor@hotmaill.com>
2019-06-27 03:15:50 +0000

    Consistent Ordering

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -5,15 +5,19 @@ RSpec.describe TasksController, type: :controller do
     it "should list the tasks in the database" do
       task1 = FactoryBot.create(:task)
       task2 = FactoryBot.create(:task)
+      task1.update_attributes(title: "Something else")
       get :index
       expect(response).to have_http_status :success
       response_value = ActiveSupport::JSON.decode(@response.body)
       expect(response_value.count).to eq(2)
+      response_ids = response_value.collect do |task|
+        task["id"]
+      end
+      expect(response_ids).to eq([task1.id, task2.id])
     end
   end
-
-  describe "task#update" do
-    it "should allow tasks to be marked as done" do 
+  describe "tasks#update" do
+    it "should allow tasks to be marked as done" do
       task = FactoryBot.create(:task, done: false)
       put :update, params: {id: task.id, task: { done: true }}
       expect(response).to have_http_status(:success)
